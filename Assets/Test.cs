8c4fa75411ad09fbfc53473138bdb7397e29674e
Nik Rychagov <zerganrus@gmail.com>
2020-09-20 20:58:04 +0300

    Added third dimension

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -5,11 +5,11 @@ public class Test : MonoBehaviour
 {
 
     public float radius = 1;
-    public Vector2 regionSize = Vector2.one;
-    public int  rejectionSamples = 30; 
+    public Vector3 regionSize = Vector3.one;
+    public int rejectionSamples = 30; 
     public float displayRadius = 1;
 
-    List<Vector2> points;
+    List<Vector3> points;
     
     void OnValidate() {
         points = PoissonDiskSampling.GeneratePoints(radius, regionSize, rejectionSamples);
@@ -18,7 +18,7 @@ public class Test : MonoBehaviour
     void OnDrawGizmos() {
         Gizmos.DrawWireCube(regionSize/2, regionSize);
         if (points != null) {
-            foreach (Vector2 point in points) {
+            foreach (Vector3 point in points) {
                 Gizmos.DrawSphere(point, displayRadius);
             }
         }
